a2a907c0c5154554abf4238856e8bc67eeb6cf58
Rakhshan174 <160410026+Rakhshan174@users.noreply.github.com>
2024-04-23 05:50:14 -0700

    Q 11, 12 TS/JS for 45 assignment

---
+++ main.ts
@@ -0,0 +1,21 @@
+//Names: store the number of a few your freinds in a array called name. print each persom's name by accessing
+//each elemnet inthe list'one at a time.
+
+let names: string [] = ["Ali","Zayan Ali", "RAYAN","Aizlan Ali"];
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+
+
+//task12
+//Greetings: start with the array you uses in Exerice 11, but instead of just printing each person's name
+// print a message to them. test of each message should be the same, but each message should be personalized with 
+//the person's name.
+let names : string [] = ["Ali","Zayan Ali", "RAYAN","Aizlan Ali"];
+
+let message: string = "Do you like watching a cartoon?"
+console.log(names [0] +" "+ message);
+console.log(names [1] +" "+ message);
+console.log(names [2] +" "+ message);
+console.log(names [3] +" "+ message);
